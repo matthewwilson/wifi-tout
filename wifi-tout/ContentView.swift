62a27e35adaff751e3dd97653dd25f6475257d15
Matthew Wilson <matthew.wilson@instil.co>
2020-02-03 15:18:17 +0000

    adding reachability

--- wifi-tout/ContentView.swift
+++ wifi-tout/ContentView.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ContentView: View {
     @State private var connectionFailureDetected = false
     @State private var status = "Connected to WiFi"
-    private let httpPinger = HttpPinger()
+    private let reachability = Reachability()
     private let notificationService = NotificationService()
     private let timer = Timer.publish(every: 20, on: .main, in: .common).autoconnect()
     
@@ -20,16 +20,14 @@ struct ContentView: View {
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .onReceive(timer) { _ in
                 print("Tick")
-                self.httpPinger.ping { result in
-                    if (result) {
-                        self.status = "Connected to WiFi"
-                        self.connectionFailureDetected = false
-                    } else {
-                        self.status = "Not connected to WiFi"
-                        if !self.connectionFailureDetected {
-                            self.notificationService.emitWifiDownNotification()
-                            self.connectionFailureDetected = true
-                        }
+                if (self.reachability.isConnectedToNetwork()) {
+                    self.status = "Connected to WiFi"
+                    self.connectionFailureDetected = false
+                } else {
+                    self.status = "Not connected to WiFi"
+                    if !self.connectionFailureDetected {
+                        self.notificationService.emitWifiDownNotification()
+                        self.connectionFailureDetected = true
                     }
                 }
             }
